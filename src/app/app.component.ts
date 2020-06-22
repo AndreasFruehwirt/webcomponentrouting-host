8b2702e87c8732619475980835487815de83cd13
Andreas Fruehwirt <andreas.fruehwirt@r-software.at>
2020-06-22 16:15:29 +0200

    advanced routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
+import {Location} from "@angular/common";
 
 @Component({
   selector: 'app-root',
@@ -8,10 +9,18 @@ import {Router} from "@angular/router";
 })
 export class AppComponent implements OnInit{
   title = 'dashboard-app';
-  constructor(private router: Router) {
+  constructor(private router: Router, private location: Location) {
+
   }
 
   ngOnInit() {
     this.router.initialNavigation();
+    //init router with starting path
+    this.router.navigateByUrl(this.location.path(true));
+
+    //on every route change tell router to navigate to defined route
+    this.location.subscribe(data => {
+      this.router.navigateByUrl(data.url);
+    });
   }
 }
